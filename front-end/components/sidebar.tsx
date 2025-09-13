b4c6f83824780654ec2d1c3c78cf44a6c584aa35
Sam. C <snchan1@ualberta.ca>
2025-09-13 15:15:02 -0600

    fields added for Profile

--- front-end/components/sidebar.tsx
+++ front-end/components/sidebar.tsx
@@ -10,6 +10,7 @@ export default function Sidebar() {
   const navItems = [
     { name: "Home", path: "/" },
     { name: "Applications", path: "/applications" },
+    { name: "Profile", path: "/profile"},
     { name: "Settings", path: "/settings" },
   ];
 
